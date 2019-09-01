8d983d24715d8a75899ae9f9cc9a9c2cd3c78d9e
Adhya Pranata <adhyapranata@gmail.com>
2019-09-01 21:08:54 +0700

    Lint DataType

--- src/DataType.js
+++ src/DataType.js
@@ -1,43 +1,43 @@
-export function isString(value) {
+export function isString (value) {
   return typeof value === 'string' || value instanceof String
 }
 
-export function isNumber(value) {
+export function isNumber (value) {
   return typeof value === 'number' && isFinite(value)
 }
 
-export function isFunction(value) {
+export function isFunction (value) {
   return typeof value === 'function'
 }
 
-export function isObject(value) {
+export function isObject (value) {
   return value && typeof value === 'object' && value.constructor === Object
 }
 
-export function isNull(value) {
+export function isNull (value) {
   return value === null
 }
 
-export function isUndefined(value) {
+export function isUndefined (value) {
   return typeof value === 'undefined'
 }
 
-export function isBoolean(value) {
+export function isBoolean (value) {
   return typeof value === 'boolean'
 }
 
-export function isRegExp(value) {
+export function isRegExp (value) {
   return value && typeof value === 'object' && value.constructor === RegExp
 }
 
-export function isError(value) {
+export function isError (value) {
   return value instanceof Error && typeof value.message !== 'undefined'
 }
 
-export function isDate(value) {
+export function isDate (value) {
   return value instanceof Date
 }
 
-export function isSymbol(value) {
+export function isSymbol (value) {
   return typeof value === 'symbol'
 }
